e13ad4b76e8178ea7933d91beee99c91f4723f09
EduardoHFreitas <eduardo.freitas@datacoper.com.br>
2016-03-22 21:46:28 -0300

    Primeiro Commit

---
+++ FXPrimeiraAula/src/br/TPrincipalController.java
@@ -0,0 +1,21 @@
+package br;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TPrincipalController {
+
+    @FXML
+    private Button Executar;
+
+    @FXML
+    private TextField Campo;
+
+    @FXML
+    void Executar(ActionEvent event) {
+
+    }
+
+}
